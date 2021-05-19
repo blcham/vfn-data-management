5e16cc026d6a01c694d86f7fa93cf89199a1e2ef
Miroslav Blasko <blcham@gmail.com>
2021-05-19 18:13:00 +0200

    [Fix] Encode url directly by curl

--- bin/deploy-form.sh
+++ bin/deploy-form.sh
@@ -20,24 +20,6 @@ OUTPUT_DIR=./target/generated-forms
 
 ENTITY_NAME=$1
 
-urlencode() {
-    # urlencode <string>
-
-    old_lc_collate=$LC_COLLATE
-    LC_COLLATE=C
-
-    local length="${#1}"
-    for (( i = 0; i < length; i++ )); do
-        local c="${1:$i:1}"
-        case $c in
-            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
-            *) printf '%%%02X' "'$c" ;;
-        esac
-    done
-
-    LC_COLLATE=$old_lc_collate
-}
-
 echo "==================================="
 echo "INFO: s-pipes url $SPIPES_SERVICE"
 echo "INFO: rdf4j-server url $RDF4J_SERVER"
@@ -48,19 +30,18 @@ echo "==================================="
 mkdir -p $OUTPUT_DIR
 
 FILE=$OUTPUT_DIR/form-$ENTITY_NAME.json
-
-
 FORM_TEMPLATE="https://slovník.gov.cz/datový/$ENTITY_NAME/form-template"
-
-
-FT="`urlencode $FORM_TEMPLATE`"
-S="`urlencode $RDF4J_SERVER`"
-SGOV_SERVICE_URL_ENCODED="`urlencode $SGOV_SERVICE_URL`"
-
-URL="$SPIPES_SERVICE/service?_pId=deploy-sample&sgovRepositoryUrl=$SGOV_SERVICE_URL_ENCODED&formGenRepositoryUrl=$S%2Frepositories%2Fofn-record-manager-formgen&recordGraphId=http%3A%2F%2Fvfn.cz%2Fontologies%2Fstudy-manager%2FformGen-sample&repositoryUrl=$S%2Frepositories%2Fofn-record-manager-app&_DformTemplate=$FT&_DexecutionId=0"
 
 set -x
 
-curl  -H 'Accept: text/json, text/plain, */*'  "$URL" > $FILE
+curl -G -H 'Accept: text/json, text/plain, */*' \
+	--data-urlencode "_pId=deploy-sample" \
+	--data-urlencode "sgovRepositoryUrl=$SGOV_SERVICE_URL" \
+	--data-urlencode "formGenRepositoryUrl=$RDF4J_SERVER/repositories/ofn-record-manager-formgen" \
+	--data-urlencode "recordGraphId=http://vfn.cz/ontologies/study-manager/formGen-sample" \
+	--data-urlencode "repositoryUrl=$RDF4J_SERVER/repositories/ofn-record-manager-app" \
+	--data-urlencode "_DformTemplate=$FORM_TEMPLATE" \
+	--data-urlencode "_DexecutionId=0" \
+	"$SPIPES_SERVICE/service" > $FILE
 
 set -x
