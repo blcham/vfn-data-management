1d9fb4bc48b0e58d1750d926825c7f8169a3f4ab
Miroslav Blasko <blcham@gmail.com>
2021-06-09 10:51:11 +0200

    [Upd] Pull repository changes on redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
-echo $CONTEXT
-
-exit
-
-
-#!/bin/bash
 # ./redeploy.sh
 #   - redeploys all services of the assembly line
 # ./redeploy.sh <SERVICE>
-#   - redeploys <SERVICE>, e.g. ./redeploy al-nginx
+#   - redeploys <SERVICE>, e.g. ./redeploy dm-ofn-record-manager
 # ENV_FILE=.env.development ./redeploy.sh <SERVICE>
 #   - redeploys <SERVICE>, e.g. ./redeploy dm-ofn-record-manager, using the properties from the .env.development file
+DIR=$(dirname $(readlink -m $0))
+cd $DIR
+git pull
+exit
+
+
 ENV_FILE="${ENV_FILE:-.env.local}"
 SERVICE="${1:-}"
 cat github.auth | docker login -u blcham --password-stdin docker.pkg.github.com
